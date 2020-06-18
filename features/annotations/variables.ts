e29b3de3d22ba68fb12fbb9cd6d78d383f9807cc
Nicholas Feitel <Nicholas.Feitel@gmail.com>
2020-06-18 19:19:25 -0400

    pushing up type annotations

---
+++ features/annotations/variables.ts
@@ -0,0 +1,11 @@
+let apples: number = 3;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+//Built-in Objects
+
+let now: Date = new Date();
+
+let colors: string[] = ['red', 'green', 'blue'];
