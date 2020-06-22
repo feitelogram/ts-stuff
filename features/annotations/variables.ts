de771df92fa388d7c509635e1a3665f1b8f2cef4
Nicholas Feitel <Nicholas.Feitel@gmail.com>
2020-06-21 22:00:28 -0400

    more

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -9,3 +9,31 @@ let nothing: undefined = undefined;
 let now: Date = new Date();
 
 let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, true];
+
+class Car {}
+
+let car: Car = new Car();
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+const logNumber: (i: number) => void = (val: number) => {
+  console.log(val);
+};
+
+const json = '{"x": 10, "y": 20}';
+const coords: { x: number; y: number } = JSON.parse(json);
+console.log(coords);
+
+let words: string[] = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') foundWord = true;
+}
+
+let numbers = [110, 1, 12];
