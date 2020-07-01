724e96e7b2953eb614861d03fc4c2fe6207f785f
Nicholas Feitel <Nicholas.Feitel@gmail.com>
2020-07-01 02:21:03 -0400

    for grider

--- features/arrays.ts
+++ features/arrays.ts
@@ -1,16 +1,14 @@
-const { log } = console;
-
 const carMakers = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 const carsByMake = [['f150'], ['corolla'], ['camaro']];
 
 //help with inference when extracting values
 
-const car = carMakers[0];
+const oneCar = carMakers[0];
 const myCar = carMakers.pop();
 
 //help with map with autocomplete!
 
-log(carMakers.map((car) => car + '!'));
+console.log(carMakers.map((car) => car + '!'));
 
 const importantDates: (string | Date)[] = [new Date(), '2020'];
