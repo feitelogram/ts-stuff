4d1068a7930a92cb72e704b5b9a2087ec6bb1503
Nicholas Feitel <Nicholas.Feitel@gmail.com>
2020-07-07 11:40:42 -0400

    final sort

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,22 +1,18 @@
-import { Sorter } from './Sorter';
+// import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const nums = new NumbersCollection([5, 0, -1, 32, 15]);
-// const sorter = new Sorter(nums);
-// sorter.sort();
-// console.log(sorter.collection);
-// const str = new CharactersCollection('XaVeWc');
-// const sorter2 = new Sorter(str);
-// sorter2.sort();
-// console.log(str.data);
-
+const nums = new NumbersCollection([5, 3, -1, 2]);
+const chars = new CharactersCollection('avocado');
 const ll = new LinkedList();
-ll.add(500);
+ll.add(5);
 ll.add(-1);
-ll.add(2);
-ll.add(25);
-const sorter = new Sorter(ll);
-sorter.sort();
+ll.add(2000);
+ll.add(-25);
+nums.sort();
+chars.sort();
+ll.sort();
+console.log(nums.data);
+console.log(chars.data);
 ll.print();
